ec69973615ad3dfa367381812d299f068f4d2cae
KwiiKss <lrent.kevin@gmail.com>
2026-05-11 15:15:22 +0200

    feat : add endpoint creation of flight

--- technical-test-api/src/main/java/technical/test/api/facade/FlightFacade.java
+++ technical-test-api/src/main/java/technical/test/api/facade/FlightFacade.java
@@ -7,6 +7,7 @@ import reactor.core.publisher.Mono;
 import technical.test.api.mapper.AirportMapper;
 import technical.test.api.mapper.FlightMapper;
 import technical.test.api.record.AirportRecord;
+import technical.test.api.record.FlightRecord;
 import technical.test.api.representation.FlightRepresentation;
 import technical.test.api.services.AirportService;
 import technical.test.api.services.FlightService;
@@ -14,6 +15,7 @@ import technical.test.api.services.FlightService;
 @Component
 @RequiredArgsConstructor
 public class FlightFacade {
+
     private final FlightService flightService;
     private final AirportService airportService;
     private final FlightMapper flightMapper;
@@ -21,15 +23,35 @@ public class FlightFacade {
 
     public Flux<FlightRepresentation> getAllFlights() {
         return flightService.getAllFlights()
-                .flatMap(flightRecord -> airportService.findByIataCode(flightRecord.getOrigin())
-                        .zipWith(airportService.findByIataCode(flightRecord.getDestination()))
-                        .flatMap(tuple -> {
-                            AirportRecord origin = tuple.getT1();
-                            AirportRecord destination = tuple.getT2();
-                            FlightRepresentation flightRepresentation = this.flightMapper.convert(flightRecord);
-                            flightRepresentation.setOrigin(this.airportMapper.convert(origin));
-                            flightRepresentation.setDestination(this.airportMapper.convert(destination));
-                            return Mono.just(flightRepresentation);
-                        }));
+                .flatMap(this::enrichFlight);
+    }
+
+    public Mono<FlightRepresentation> createFlight(FlightRepresentation flightRepresentation) {
+
+        FlightRecord flightRecord = flightMapper.convert(flightRepresentation);
+
+        if (flightRecord.getId() == null) {
+            flightRecord.setId(java.util.UUID.randomUUID());
+        }
+
+        return flightService.saveFlight(flightRecord)
+                .flatMap(this::enrichFlight);
+    }
+
+    private Mono<FlightRepresentation> enrichFlight(FlightRecord flightRecord) {
+
+        return airportService.findByIataCode(flightRecord.getOrigin())
+                .zipWith(airportService.findByIataCode(flightRecord.getDestination()))
+                .map(tuple -> {
+
+                    AirportRecord origin = tuple.getT1();
+                    AirportRecord destination = tuple.getT2();
+
+                    FlightRepresentation response = flightMapper.convert(flightRecord);
+                    response.setOrigin(airportMapper.convert(origin));
+                    response.setDestination(airportMapper.convert(destination));
+
+                    return response;
+                });
     }
 }
